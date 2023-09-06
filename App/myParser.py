9ebe332327f49c146756dea3ffd97171ee4438d0
AlisonAristizabal <a.aristizabalg@uniandes.edu.co>
2023-09-05 20:26:31 -0500

    rulesrepeat

--- App/myParser.py
+++ App/myParser.py
@@ -1,4 +1,4 @@
-
+import myLexer as lxr
 def parse(tokens, vars, valueVars, proc, parametros_procedimientos):
     #valid = False
 
@@ -12,20 +12,30 @@ def parse(tokens, vars, valueVars, proc, parametros_procedimientos):
 
     palabrasReservadas = [defWords, symbols, simpleCommands, conditional, loop, repeatTimes, conditions]
 
+    validCommands = False
+    validConditionals = False
+    validLoops = False
+    validrepeat = False
+    validConditions = False
     for token in range(0,len(tokens)):
-        if tokens[token]:
+        if lxr.NotIn_palabrasreservadas(tokens[token],simpleCommands) == False:
+            validCommands = rulesSimpleCommands(tokens,token)
+        elif lxr.NotIn_palabrasreservadas(tokens[token],conditional) == False:
+            validConditionals = rulesConditionals(tokens,token)
+        elif lxr.NotIn_palabrasreservadas(tokens[token],loop) == False:
+            validLoops = rulesloops(tokens,token)
+        elif tokens[token] == "repeat":
+            validrepeat = rulesRepeat(tokens,token)
+        elif lxr.NotIn_palabrasreservadas(tokens[token],conditions) == False:
+            validConditions = rulesConditions(tokens,token)
+        else:
             pass
 
     #falta hacer que dependiendo del token quee lea ejecute alguna de las de abajo y pues cada una de esas verifica algo.
     #por ejemplo, si el token que encuentra es un commando toca revisar la sintaxis de dicho comando. entnoces toca escibir las funciones que revisan de cada cosa.
     # por ejemplo  en el de rulessimplecomand tocaria hacer muchos ifs para cada palabra reservada dependiendo de la sintaxis. 
-
-    validCommands = rulesSimpleCommands()
-    validConditionals = rulesConditionals()
-    validLoops = rulesloops()
-    validrepeat = rulesRepeat()
-    validConditions = rulesConditions()
-    validBlocks = rulesBlocks()
+    
+    validBlocks = rulesBlocks(tokens)
 
     if validCommands and validConditionals and validLoops and validrepeat and validConditions and validBlocks:
         valid = True
@@ -34,33 +44,41 @@ def parse(tokens, vars, valueVars, proc, parametros_procedimientos):
 
     return valid
 
-def rulesSimpleCommands():
+def rulesSimpleCommands(lista,posicion):
     pass
 
-def rulesConditionals():
+def rulesConditionals(lista,posicion):
     pass
 
-def rulesloops():
+def rulesloops(lista,posicion):
     pass
 
-def rulesRepeat():
-    pass
+def rulesRepeat(lista,posicion):
+    try:
+        value = int(lista[posicion+1])
+    except:
+        pass
+    times=lista[posicion+2]
+    llave=lista[posicion+3]
+    if (isinstance(value,int) == True) and (times == "times") and (llave == "{"):
+        return True
+    
 
-def rulesConditions():
+def rulesConditions(lista,posicion):
     pass
 
 def rulesBlocks(tokens):
-    corchete_a = 0
-    corchete_c=0
+    llave_a = 0
+    llave_c=0
     for token in tokens:
         if token == "{":
-            corchete_a += 1
+            llave_a += 1
         elif token == "}":
-            corchete_c += 1
+            llave_c += 1
         else:
-            corchete_a = corchete_a
-            corchete_c = corchete_c
-    if corchete_c == corchete_a:
+            llave_a = llave_a
+            llave_c = llave_c
+    if llave_c == llave_a:
         return True
     else:
         return False
